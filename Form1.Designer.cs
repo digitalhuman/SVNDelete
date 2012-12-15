1ffb3b3d2d761b74a157d772751eb432bf9ea86b
Digital Human <vangelier@hotmail.com>
2012-12-15 07:50:11 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SVNDelete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.cmdSelect = new System.Windows.Forms.Button();
+            this.lstList = new System.Windows.Forms.ListBox();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.cmdSelect);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lstList);
+            this.splitContainer1.Size = new System.Drawing.Size(541, 329);
+            this.splitContainer1.SplitterDistance = 84;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // cmdSelect
+            // 
+            this.cmdSelect.Location = new System.Drawing.Point(12, 12);
+            this.cmdSelect.Name = "cmdSelect";
+            this.cmdSelect.Size = new System.Drawing.Size(193, 61);
+            this.cmdSelect.TabIndex = 1;
+            this.cmdSelect.Text = "Select root directory van het project";
+            this.cmdSelect.UseVisualStyleBackColor = true;
+            this.cmdSelect.Click += new System.EventHandler(this.cmdSelect_Click_1);
+            // 
+            // lstList
+            // 
+            this.lstList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstList.FormattingEnabled = true;
+            this.lstList.Location = new System.Drawing.Point(0, 0);
+            this.lstList.Name = "lstList";
+            this.lstList.Size = new System.Drawing.Size(541, 236);
+            this.lstList.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 329);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SVN Remover";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button cmdSelect;
+        private System.Windows.Forms.ListBox lstList;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace SVNDelete
+{
+    public partial class Form1 : Form
+    {
+        public void addListItem(string item)
+        {
+            if (lstList.InvokeRequired)
+            {
+                BeginInvoke(new MethodInvoker(delegate() { addListItem(item); }));
+            }
+            else
+            {
+                lstList.Items.Add(item);
+            }
+        }
+
+        WindowsIdentity self = WindowsIdentity.GetCurrent();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void parseDir(String path)
+        {
+            foreach (String f in Directory.GetDirectories(path))
+            {
+                if (f.EndsWith(".svn"))
+                {
+                    DirectoryInfo dinfo;
+                    try
+                    {
+                        DirectorySecurity ds = new DirectorySecurity(f, AccessControlSections.Access);
+                        FileSystemAccessRule fsRule = new FileSystemAccessRule(self.Name,
+                            FileSystemRights.FullControl,
+                            AccessControlType.Allow);
+                        ds.SetAccessRule(fsRule);
+
+                        dinfo = new DirectoryInfo(f);
+                        dinfo.Attributes = FileAttributes.Normal;
+                        dinfo.SetAccessControl(ds);
+                    }
+                    catch (Exception err)
+                    {
+                    }
+
+                    dinfo = new DirectoryInfo(f);
+                    DirectorySecurity sec = dinfo.GetAccessControl();
+                    sec.SetOwner(self.Owner);
+
+                    setFileSecurity(f);
+
+                    addListItem(f);
+                    Application.DoEvents();
+
+                    try
+                    {
+                        Directory.Delete(f, true);
+                    }
+                    catch (Exception err)
+                    {
+                        MessageBox.Show(err.Message);
+                    }
+                }
+                else
+                {
+                    //addListItem(f);
+                    Application.DoEvents();
+                    parseDir(f);
+                }
+            }
+        }
+
+        public void setFileSecurity(String path)
+        {
+            FileSecurity fs;
+            FileSystemAccessRule fsRule = new FileSystemAccessRule(self.Name, FileSystemRights.FullControl, AccessControlType.Allow);
+            foreach (String file in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories))
+            {
+                try
+                {
+                    fs = new FileSecurity(file, AccessControlSections.Access);
+                    fs.AddAccessRule(fsRule);
+                    File.SetAccessControl(file, fs);
+                    File.SetAttributes(file, FileAttributes.Normal);
+                    fs.SetOwner(self.Owner);
+                }
+                catch (Exception err)
+                {
+                }
+
+                FileInfo finfo = new FileInfo(file);
+                finfo.Attributes = FileAttributes.Normal;
+                FileSecurity sec = finfo.GetAccessControl();
+                sec.SetAccessRule(fsRule);
+                sec.SetOwner(self.Owner);
+
+                Application.DoEvents();
+                try
+                {
+                    File.Delete(file);
+                    Application.DoEvents();
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+                Application.DoEvents();
+            }            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdSelect_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog dlg = new FolderBrowserDialog();
+                dlg.RootFolder = Environment.SpecialFolder.MyComputer;
+                dlg.Description = "Selecteer de directory";
+                DialogResult res = dlg.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    String path = dlg.SelectedPath;
+                    lstList.Items.Clear();
+                    cmdSelect.Enabled = false;
+                    Application.DoEvents();
+                    parseDir(path);
+                }
+                cmdSelect.Enabled = true;
+                MessageBox.Show("Ok done!");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
